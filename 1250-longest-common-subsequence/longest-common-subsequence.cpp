c8489c1bffb495637f9e029d2e13fc60f646d01c
gautamkshah <109517113+gautamkshah@users.noreply.github.com>
2024-10-13 18:08:21 +0530

    Time: 20 ms (73.75%) | Memory: 25.8 MB (75.47%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
+    int n,m;
+    int solve(string & t1,string &t2,int i,int j){
+        if(i==n || j==m) return 0;
+        if(t1[i]==t2[j]){
+            return 1+solve(t1,t2,i+1,j+1);
+        }
+        return max(solve(t1,t2,i+1,j),solve(t1,t2,i,j+1));
+    }
+    int longestCommonSubsequence(string t1, string t2) {
+        n=t1.size();
+        m=t2.size();
+       
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         
-        // Create a DP table with (n+1) x (m+1) size
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        
-        // Fill the DP table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                // If characters match, take diagonal value and add 1
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    // If they don't match, take the max of top or left cell
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(t1[i-1]==t2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        
-        // The bottom-right corner contains the length of the longest common subsequence
         return dp[n][m];
+        
     }
 };
