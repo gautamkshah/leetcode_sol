b5d32d6a7b108e33ffd22e26289002bfd7b92ac8
gautamkshah <109517113+gautamkshah@users.noreply.github.com>
2025-01-12 13:13:49 +0530

    Time: 191 ms (92.86%) | Memory: 238.8 MB (100.00%) - LeetSync

--- 3720-minimize-the-maximum-edge-weight-of-graph/minimize-the-maximum-edge-weight-of-graph.cpp
+++ 3720-minimize-the-maximum-edge-weight-of-graph/minimize-the-maximum-edge-weight-of-graph.cpp
@@ -1,26 +1,50 @@
-vector<pair<int,int>>adj[(int) 1e5];
-vector<bool>vis;
-int dfs(int node, int m){
-     vis[node] = 1;
-     int ret = 1;
-     for(auto [a,b] : adj[node])
-          if(!vis[a] && b <= m)ret += dfs(a, m);
+#include <vector>
+#include <utility>
+using namespace std;
 
-     return ret;
+vector<pair<int, int>> adj[(int)1e5];
+vector<bool> vis;
+
+void dfs(int node, int m) {
+    vis[node] = true;
+    for (auto [a, b] : adj[node]) {
+        if (!vis[a] && b <= m) {
+            dfs(a, m);
+        }
+    }
 }
+
 class Solution {
 public:
     int minMaxWeight(int n, vector<vector<int>>& edges, int threshold) {
-        for(int i = 0; i < n; i++)adj[i].clear();
-        for(auto i : edges)
-            adj[i[1]].push_back({i[0], i[2]});
-        
-        int l = 1 ,r = 1e6, m;
-        while(l <= r){
-            m = (l + r) >> 1;
-            vis = vector<bool>(n);
-            (dfs(0, m) != n ? l = m + 1 : r = m - 1);
+        for (int i = 0; i < n; i++) adj[i].clear();
+        for (auto& edge : edges) {
+            adj[edge[1]].emplace_back(edge[0], edge[2]); // Reverse graph for reachability to node 0
         }
-        return (l == 1e6 + 1 ? -1 : l);
+
+        int l = 1, r = 1e6, m;
+        while (l <= r) {
+            m = (l + r) >> 1;
+
+            vis = vector<bool>(n, false); // Reset the visited array for each mid value
+            dfs(0, m); // Perform DFS starting from node 0
+
+            // Check if all nodes are visited
+            bool allVisited = true;
+            for (int i = 0; i < n; i++) {
+                if (!vis[i]) {
+                    allVisited = false;
+                    break;
+                }
+            }
+
+            if (allVisited) {
+                r = m - 1; // Try to minimize the maximum edge weight
+            } else {
+                l = m + 1; // Increase the weight limit
+            }
+        }
+
+        return (l > 1e6 ? -1 : l);
     }
 };
