c4cf325474ba44128c771e93d20472bcd7111bdc
gautamkshah <109517113+gautamkshah@users.noreply.github.com>
2025-04-18 21:55:01 +0530

    Time: 110 ms (84.31%) | Memory: 49.3 MB (91.40%) - LeetSync

--- 472-concatenated-words/concatenated-words.cpp
+++ 472-concatenated-words/concatenated-words.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        unordered_set<string> dictionary(words.begin(), words.end());
-        vector<string> answer;
-        for (const string& word : words) {
-            const int length = word.length();
-            vector<bool> dp(length + 1);
+        unordered_set<string> st(words.begin(), words.end());
+        vector<string> ans;
+        for (string& word : words) {
+            int len = word.length();
+            vector<bool> dp(len + 1, false);
             dp[0] = true;
-            for (int i = 1; i <= length; ++i) {
-                for (int j = (i == length ? 1 : 0); !dp[i] && j < i; ++j) {
-                    dp[i] = dp[j] && dictionary.count(word.substr(j, i - j));
+            for (int i = 1; i <= len; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (i == len and j == 0) {
+                        continue;
+                    }
+                    if (dp[j] && st.count(word.substr(j, i - j))) {
+                        dp[i] = true;
+                        break;
+                    }
                 }
             }
-            if (dp[length]) {
-                answer.push_back(word);
+            if (dp[len]) {
+                ans.push_back(word);
             }
         }
-        return answer;
+
+        return ans;
     }
 };
