6ad69abab111c8f8d83344916d4e3bc673c97bac
gautamkshah <109517113+gautamkshah@users.noreply.github.com>
2026-02-19 20:00:36 +0530

    Time: 41 ms (80.62%) | Memory: 43.3 MB (48.26%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.cpp
+++ 480-sliding-window-median/sliding-window-median.cpp
@@ -1,35 +1,28 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-using namespace __gnu_pbds;
-
-typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, 
-             tree_order_statistics_node_update> ordered_set;
 class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-    ordered_set window;
-    vector<double> result;
-
-    for (int i = 0; i < nums.size(); ++i) {
-        
-        window.insert({nums[i], i});
-
-        if (window.size() >= k) {
-            
-            if (k % 2 != 0) {
-                result.push_back(window.find_by_order(k / 2)->first);
-            } else {
-               
-                double m1 = window.find_by_order(k / 2)->first;
-                double m2 = window.find_by_order(k / 2 - 1)->first;
-                result.push_back((m1 + m2) / 2.0);
+        int n = nums.size();
+        multiset<int> st(nums.begin(), nums.begin() + k);
+        auto it = next(st.begin(), k / 2);
+        vector<double> ans;
+        auto getMedian = [&]() {
+            if (k % 2 != 0)
+                return (double)*it;
+            return ((double)*it + *prev(it)) * 0.5;
+        };
+        ans.push_back(getMedian());
+        for (int i = k; i < n; i++) {
+            st.insert(nums[i]);
+            if (nums[i] < *it) {
+                it--;
             }
-
-           
-            window.erase({nums[i - k + 1], i - k + 1});
+            int ot = nums[i - k];
+            if (ot <= *it) {
+                it++;
+            }
+            st.erase(st.find(ot));
+            ans.push_back(getMedian());
         }
+        return ans;
     }
-    return result;
-}
 };
