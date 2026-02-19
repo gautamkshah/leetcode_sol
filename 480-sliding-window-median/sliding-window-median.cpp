dbc1bfb9d772aad06fb83a464ecb7e117132fdf8
gautamkshah <109517113+gautamkshah@users.noreply.github.com>
2026-02-19 20:39:39 +0530

    Time: 47 ms (69.84%) | Memory: 42.9 MB (62.17%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.cpp
+++ 480-sliding-window-median/sliding-window-median.cpp
@@ -5,10 +5,9 @@ public:
         multiset<int> st(nums.begin(), nums.begin() + k);
         auto it = next(st.begin(), k / 2);
         vector<double> ans;
-        auto getMedian = [&]() {
-            if (k % 2 != 0)
-                return (double)*it;
-            return ((double)*it + *prev(it)) * 0.5;
+        auto getMedian =[&](){
+            if(k%2!=0) return (double)*it;
+            else return ((double)*it + *prev(it))*0.5;
         };
         ans.push_back(getMedian());
         for (int i = k; i < n; i++) {
