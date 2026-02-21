c9dea97c50eff58bfd2b9dc8eeedf3aa58c2b76b
gautamkshah <109517113+gautamkshah@users.noreply.github.com>
2026-02-21 12:26:52 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (91.35%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,13 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k%=n; // the value is k ..0..n-1
-        vector<int>ans;
-        for(int i=n-k;i<n;++i)ans.push_back(nums[i]);
-        //we are getting the last k elements
-        //(n-k)(k)
-        //(k)(n-k)
-        for(int i=0;i<(n-k);++i)ans.push_back(nums[i]);
-        nums = ans;
-
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
