40f9bb5081883c1e62a03382fc2fd9146cd9eed2
gautamkshah <109517113+gautamkshah@users.noreply.github.com>
2026-03-15 13:23:09 +0530

    Time: 2 ms (50.69%) | Memory: 8.6 MB (65.28%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(int x) {
         if(x<0) return false;
         int copy=x;
-        long long int rev=0;
+        long int rev=0;
         while(x>0){
             rev=rev*10+x%10;
             x/=10;
